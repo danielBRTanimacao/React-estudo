f97d4b10f5d81d5e58d000dea470c6cbaf2cd60c
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-12-13 12:50:30 -0300

    index app

--- first-react-project/src/App.jsx
+++ first-react-project/src/App.jsx
@@ -1,8 +1,26 @@
+import { useRef } from 'react'
+
+// React Hooks ferramentas para auxiliar
+// useRef
 
 function Home() {
+  const input_text = useRef()
+
+  function additingItens() {
+
+  }
   return (
-    <div>
-      <h1>Lista de tarefas</h1>
+    <div class="container pt-5 pb-5 mt-5 bg-dark">
+      <h1 class="display-4 text-light">Lista de compras</h1>
+      <div class="input-group w-50 pt-2">
+        <input class="form-control" type="text" placeholder="Produto..." ref={input_text}/>
+        <button class="btn btn-primary" type="button" onClick={additingItens()}>Adicionar</button>
+      </div>
+      <div class="bg-light mt-5 w-50">
+        <div class="p-2">
+          a
+        </div>
+      </div>
     </div>
   )
 }
