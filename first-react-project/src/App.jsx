addf61457ddabafe449bc471c5f288bf6a438984
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-12-15 11:52:42 -0300

    bugs react

--- first-react-project/src/App.jsx
+++ first-react-project/src/App.jsx
@@ -1,28 +1,34 @@
-import { useRef } from 'react'
+import { useRef } from "react";
 
 // React Hooks ferramentas para auxiliar
 // useRef
 
 function Home() {
-  const input_text = useRef()
+    const input_text = useRef();
 
-  function additingItens() {
-
-  }
-  return (
-    <div class="container pt-5 pb-5 mt-5 bg-dark">
-      <h1 class="display-4 text-light">Lista de compras</h1>
-      <div class="input-group w-50 pt-2">
-        <input class="form-control" type="text" placeholder="Produto..." ref={input_text}/>
-        <button class="btn btn-primary" type="button" onClick={additingItens()}>Adicionar</button>
-      </div>
-      <div class="bg-light mt-5 w-50">
-        <div class="p-2">
-          a
+    function additingItens() {
+        let text_input = document.querySelector("p#txtinput");
+        text_input.innerHTML = input_text.current.value;
+    }
+    return (
+        <div class="container pt-5 pb-5 mt-5 bg-dark">
+            <h1 class="display-4 text-light">Lista de compras</h1>
+            <div class="input-group w-50 pt-2">
+                <input
+                    class="form-control"
+                    type="text"
+                    placeholder="Produto..."
+                    ref={input_text}
+                />
+                <button class="btn btn-primary" onClick={additingItens}>
+                    Adicionar
+                </button>
+            </div>
+            <div class="bg-light mt-5 w-50">
+                <p class="p-2" id="txtinput"></p>
+            </div>
         </div>
-      </div>
-    </div>
-  )
+    );
 }
 
-export default Home
+export default Home;
