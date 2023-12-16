3149057dff6573e20cbd32ae877b689569bd66f4
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-12-16 17:55:51 -0300

    update aula index

--- first-react-project/src/App.jsx
+++ first-react-project/src/App.jsx
@@ -1,17 +1,22 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import { v4 } from "uuid";
 
 // React Hooks ferramentas para auxiliar
 // useRef
+// useState / estado -> É uma variavel que quando os valores são trocados a tela ira "recarregar os valores"
 
 function Home() {
+    const [products, setProducts] = useState([]);
     const input_text = useRef();
 
     function additingItens() {
-        let text_input = document.querySelector("p#txtinput");
-        text_input.innerHTML = input_text.current.value;
+        setProducts({ id: v4(), name: input_text.current.value }, ...products);
+        // let text_input = document.querySelector("p#txtinput");
+        // products.push(input_text.current.value);
+        // text_input.innerHTML = products;
     }
     return (
-        <div class="container pt-5 pb-5 mt-5 bg-dark">
+        <div class="container pt-5 pb-5 mt-5 bg-secondary">
             <h1 class="display-4 text-light">Lista de compras</h1>
             <div class="input-group w-50 pt-2">
                 <input
@@ -24,9 +29,14 @@ function Home() {
                     Adicionar
                 </button>
             </div>
-            <div class="bg-light mt-5 w-50">
+            {products.map((product) => (
+                <div class="text-light bg-primary" key={product.id}>
+                    {product.name}
+                </div>
+            ))}
+            {/* <div class="bg-light mt-5 w-50">
                 <p class="p-2" id="txtinput"></p>
-            </div>
+            </div> */}
         </div>
     );
 }
