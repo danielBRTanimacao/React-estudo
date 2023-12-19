3c2e4d5ff911924be4b53d96d18a3c0cc6e9b8ef
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-12-19 08:48:06 -0300

    aula data dentro components

--- react-02/src/components/Data.jsx
+++ react-02/src/components/Data.jsx
@@ -5,10 +5,20 @@ export default function Data() {
         email: nome + "@gmail.com"
     };
 
+    let a = 12;
+    let b = 23;
+
+    function add(a, b) {
+        return a + b;
+    }
+
     return (
         <>
             <p>Nome do cliente: {cliente.name}</p>
             <p>Email do cliente: {cliente.email}</p>
+            <p>
+                A soma de {a} + {b} = {add(a, b)}
+            </p>
         </>
     );
 }
