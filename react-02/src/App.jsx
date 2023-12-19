539968e977e97ceda2577e88894b8b2677441b75
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-12-19 09:29:53 -0300

    css em reactjs

--- react-02/src/App.jsx
+++ react-02/src/App.jsx
@@ -1,14 +1,15 @@
 import React from "react";
+import Dados from "./components/Dados";
 
-// Componentes
-import Data from "./components/Data";
+import "./assets/css/Style.css";
 
 export default function App() {
     // Todos os componentes devem começar com letra maiuscula
     return (
         <>
-            <h3>Testes de componentes React</h3>
-            <Data />
+            <h1>Aplicando css</h1>
+            <h2 className="cor-text">Mais textos com css</h2>
+            <Dados />
         </>
     );
 }
---
+++ react-02/src/components/Dados.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "../assets/css/Dados.css";
+
+export default function Dados() {
+    return (
+        <>
+            <h1>Dados</h1>
+        </>
+    );
+}
