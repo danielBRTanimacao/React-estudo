c5da5c0c98144d83720d62c7ac5f2b5ee0444666
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-12-23 12:21:08 -0300

    Update aula mutiplicação de components

--- react-03-props/src/App.jsx
+++ react-03-props/src/App.jsx
@@ -5,21 +5,59 @@ import Article from "./components/Article";
 export default function App() {
     let array = [
         {
+            id: 1,
             title: "Um",
             subtitle: "Sub Um"
         },
         {
+            id: 2,
             title: "Dois",
             subtitle: "Sub dois"
+        },
+        {
+            id: 3,
+            title: "Três",
+            subtitle: "Sub três"
+        },
+        {
+            id: 4,
+            title: "Quatro",
+            subtitle: "Sub quatro"
+        },
+        {
+            id: 5,
+            title: "Cinco",
+            subtitle: "Sub cinco"
+        },
+        {
+            id: 6,
+            title: "Seis",
+            subtitle: "Sub seis"
+        },
+        {
+            id: 7,
+            title: "Sete",
+            subtitle: "Sub sete"
+        },
+        {
+            id: 7,
+            title: "Oito",
+            subtitle: "Sub oito"
         }
     ];
+
+    let conteudo = array.map((artigos) => (
+        <Article
+            id={artigos.id}
+            titulo={artigos.title}
+            subtitulo={artigos.subtitle}
+        />
+    ));
 
     return (
         <>
             <h1>Hello</h1>
-            <Article title={array[0].title} subtitle={array[0].subtitle} />
-            <Article title={array[1].title} subtitle={array[1].subtitle} />
-            <Article title="Noticia dois" subtitle="bico seco Papoca" />
+            {conteudo}
         </>
     );
 }
--- react-03-props/src/components/Article.jsx
+++ react-03-props/src/components/Article.jsx
@@ -1,8 +1,8 @@
-export default function Article({ title, subtitle }) {
+export default function Article({ titulo, subtitulo }) {
     return (
         <>
-            <h3>{title}</h3>
-            <h4>{subtitle}</h4>
+            <h3>{titulo}</h3>
+            <h4>{subtitulo}</h4>
             <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                 eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
