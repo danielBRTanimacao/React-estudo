7f2b35f33b0ddedd9e0916c1036f265b346659d8
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-01-02 18:00:57 -0300

    Hooks states 01

--- react-04-hooks/src/App.jsx
+++ react-04-hooks/src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-import HookState from "./components/UseState01";
+import HookState01 from "./components/UseState01";
 
 export default () => {
     return (
         <>
             <h1>React Hooks</h1>
-            <HookState />
+            <HookState01 />
         </>
     );
 };
