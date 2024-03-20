d090b7264db9fa81e5e427787fcba1fdf0804edd
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-03-20 13:04:41 -0300

    new feature input usestate

--- react-04-hooks/src/components/UseState13.jsx
+++ react-04-hooks/src/components/UseState13.jsx
@@ -1,11 +1,19 @@
 import React, { useState } from "react";
 
 export default () => {
+    const [text, setText] = useState("");
+
     return (
         <>
             <h3>Exercicio 13</h3>
-            <h4>Conditional rendering - Change Component</h4>
+            <h4>Input data</h4>
             <hr />
+            <input
+                type="text"
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+            />
+            <p>{text}</p>
         </>
     );
 };
