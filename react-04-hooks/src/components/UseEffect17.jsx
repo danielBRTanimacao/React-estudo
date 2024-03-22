6dcea4c2371309a13ff0fc21c39dd874f7e54705
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-03-22 09:19:16 -0300

    useffect

--- react-04-hooks/src/components/UseEffect17.jsx
+++ react-04-hooks/src/components/UseEffect17.jsx
@@ -2,20 +2,35 @@ import React, { useState, useEffect } from "react";
 
 export default () => {
     const [value, setValue] = useState(0);
+    const [name, setName] = useState("");
 
     function incremento() {
         setValue(value + 1);
     }
 
+    function defName() {
+        setName("Daniel");
+    }
+
     useEffect(() => {
-        console.log("componente iniciado");
+        console.log("Executar apenas no arranque do componente");
+    }, []);
+
+    useEffect(() => {
+        console.log("Executar apenas na alteração do VALUE");
     }, [value]);
+
+    useEffect(() => {
+        console.log("Executar apenas na alteração do Name");
+    }, [name]);
 
     return (
         <>
             <h1>UseEffect 01</h1>
             <button onClick={incremento}>Incrementar</button>
             <h3>{value}</h3>
+            <button onClick={defName}>Definir nome</button>
+            <h3>{name}</h3>
         </>
     );
 };
